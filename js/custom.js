0041db91ced7d8c71fdfa315b433ac177c29976b
prhost <prhost@prhost.com.br>
2015-04-19 22:41:14 -0300

    adicionado codigo para marcar ativo o menu quando se passar pelo ID

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,26 @@
+$('.nav li a').click(function () {
+    $('.nav li a').removeClass("actived");
+    $(this).addClass("actived");
+});
 
-$('.nav li a').click(function(){
-	$('.nav li a').removeClass("actived");
-	$(this).addClass("actived");
+
+function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    $('nav ul.nav li a').each(function () {
+        var currLink = $(this);
+        var href = currLink.attr("href");
+        var refElement = $(href);
+        if (refElement.position().top <= (scrollPos) &&
+            refElement.position().top + refElement.height() > scrollPos) {
+            $('nav ul.nav li a').removeClass("actived");
+            currLink.addClass("actived");
+        }
+        else{
+            currLink.removeClass("actived");
+        }
+    });
+}
+
+$(document).ready(function () {
+    $(document).scroll(onScroll);
 });
